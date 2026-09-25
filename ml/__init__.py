ca00e8611819f587edd44c33620434af292744ee
Ralu6437 <rout67668@gmail.com>
2026-09-25 13:49:05 +0530

    Create __init__.py

---
+++ ml/__init__.py
@@ -0,0 +1,11 @@
+"""
+ICC Men's Cricket World Cup
+Machine Learning Package
+
+This package contains:
+- Feature preparation
+- Model training
+- Model evaluation
+- Match prediction
+- Saved machine learning models
+"""
