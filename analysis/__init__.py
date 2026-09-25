1e49a530230ccf7cdca591d4c1df9ecc715bc5a7
Ralu6437 <rout67668@gmail.com>
2026-09-25 12:07:39 +0530

    Create __init__.py

---
+++ analysis/__init__.py
@@ -0,0 +1,14 @@
+"""
+ICC Men's Cricket World Cup
+Data Analysis Package
+
+This package contains modules for:
+- Team analysis
+- Player analysis
+- Batting analysis
+- Bowling analysis
+- Match analysis
+- Statistical analysis
+- Data visualization
+- Machine learning
+"""
